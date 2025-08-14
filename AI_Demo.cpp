f18cb1d3655aaeeb9e6db0d1c4fde85f6a64a57f
nityanand.tamsoy@rampgroup.com <nityanand.tamsoy@rampgroup.com>
2025-08-14 14:13:31 +0530

    comments removed from the code

--- AI_Demo.cpp
+++ AI_Demo.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 #include <cstring>
-using namespace std;  // MISRA and AUTOSAR violation: avoid using directives in global scope
+using namespace std;  
  
 class Vehicle {
 public:
-    int* speed;  // MISRA: Avoid naked pointers, use smart pointers or references
+    int* speed;  
  
     Vehicle() {
-        speed = (int*)malloc(sizeof(int)); // AUTOSAR & MISRA violation: Use of malloc instead of new
+        speed = (int*)malloc(sizeof(int)); 
         *speed = 0;
     }
  
     void setSpeed(const char* inputSpeed) {
         char buffer[5];
-        strcpy(buffer, inputSpeed);  // SCA violation: unsafe function, buffer overflow risk
+        strcpy(buffer, inputSpeed);  
  
-        // No validation on speed input (could be non-numeric or out of range)
+       
         *speed = atoi(buffer);
     }
  
     void printSpeed() {
-        cout << "Speed is: " << *speed << endl; // MISRA/AUTOSAR violation: std::cout usage not allowed in safety-critical code
+        cout << "Speed is: " << *speed << endl;
     }
  
     ~Vehicle() {
-        free(speed);  // MISRA/AUTOSAR violation: mixing malloc/free with C++
+        free(speed);  
     }
 };
  
 int main() {
     Vehicle v;
     char input[10];
-    cin >> input; // No input validation, unsafe
+    cin >> input; 
  
     v.setSpeed(input);
  
     v.printSpeed();
  
-    return 0; // Missing return 0 from main is not an error in C++14 but good practice to include
+    return 0; 
 }
